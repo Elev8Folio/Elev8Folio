c7e37c4c1319ca17af4731853e7ce02379a792b9
Tsewanghyolmo01 <tds22@njit.edu>
2023-12-01 16:04:31 -0500

    Pushing the Folio Gram

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -5,6 +5,14 @@ function Homepage() {
     <div className="HomePageHolder">
       <div className="HomePageHeader animate__animated animate__pulse ">
         <img alt="FolioIcon" src={FolioImg} className="FolioBg"></img>
+        <div className="MakerName">
+          <p>
+            Made<span>&lt;for&gt;</span>students
+          </p>
+          <p>
+            From<span>&lt;two&gt;</span>students
+          </p>
+        </div>
       </div>
     </div>
   );
