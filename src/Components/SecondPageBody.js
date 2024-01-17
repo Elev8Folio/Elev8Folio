2e9245943d4e5970c711b4ac442924c394b9b6a0
Norsang Nyandak <TNorsang>
2024-01-17 15:59:15 -0500

    Updated logic for name inputs

--- src/Components/SecondPageBody.js
+++ src/Components/SecondPageBody.js
@@ -1,23 +1,76 @@
+import { useState } from "react";
 function SecondPageBody() {
+  const [Progression, setProgression] = useState(0);
+  let InputList = [
+    <input
+      onChange={(e) => handleChange(e)}
+      type="text"
+      placeholder="First Name"
+      id="Fname"
+    />,
+    <input
+      onChange={(e) => handleChange(e)}
+      type="text"
+      placeholder="Last Name"
+      id="Lname"
+    />,
+    <input
+      onChange={(e) => handleChange(e)}
+      type="text"
+      placeholder="University"
+      id="university"
+    />,
+    <input
+      onChange={(e) => handleChange(e)}
+      type="text"
+      placeholder="Title"
+      id="title"
+    />,
+  ];
+  const handleChange = (e) => {
+    if (e.target.id === "Fname" && e.target.value !== "" && Progression === 0)
+      setProgression(1);
+    else if (
+      e.target.id === "Lname" &&
+      e.target.value !== "" &&
+      Progression === 1
+    )
+      setProgression(2);
+    else if (
+      e.target.id === "university" &&
+      e.target.value !== "" &&
+      Progression === 2
+    )
+      setProgression(3);
+    else if (
+      e.target.id === "title" &&
+      e.target.value !== "" &&
+      Progression === 3
+    )
+      setProgression(4);
+  };
+
   const clicked = () => {
     alert("Upload my friend");
   };
 
   return (
     <div className="SecondPageBody">
-      <div className="Name">
-        <input type="text" placeholder="FirstName" />
-        <input type="text" placeholder="LastName" />
-      </div>
-      <div className="Title">
-        <input type="text" placeholder="Title" />
+      <div className="inputInfo">
+        {InputList[0]}
+        {Progression > 0 && InputList[1]}
       </div>
 
-      <div className="StepOne">
-        <p>
-          STEP 1: <span onClick={clicked}> Upload Resume </span>
-        </p>
-      </div>
+      <div className="inputInfo">{Progression > 1 && InputList[2]}</div>
+      <div className="inputInfo">{Progression > 2 && InputList[3]}</div>
+
+      {Progression > 3 && (
+        <div className="StepOne">
+          <p>
+            <span onClick={clicked}> Upload Picture </span>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
